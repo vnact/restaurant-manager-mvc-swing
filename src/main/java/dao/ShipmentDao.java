d548f97f390307a55a9bf5cf4ba6b1c989f81e63
Trần Đức Cường <clonebmn2itt@gmail.com>
2020-12-25 14:14:31 +0700

    feat: update

--- src/main/java/dao/ShipmentDao.java
+++ src/main/java/dao/ShipmentDao.java
@@ -35,7 +35,7 @@ public class ShipmentDao extends Dao<Shipment> {
     @Override
     public Shipment get(int id) throws SQLException {
         Statement statement = conn.createStatement();
-        String query = "SELECT * FROM `shipment`";
+        String query = "SELECT * FROM `shipment` WHERE `idOrder` = " + id;
         ResultSet rs = statement.executeQuery(query);
         if (rs.next()) {
             Shipment shipment = Shipment.getFromResultSet(rs);
@@ -49,9 +49,9 @@ public class ShipmentDao extends Dao<Shipment> {
     @Override
     public void save(Shipment t) throws SQLException {
         if (t == null) {
-            throw new SQLException("Customer rỗng");
+            throw new SQLException("Shipment rỗng");
         }
-        String query = "INSERT INTO `shipment` (`idOrder`, `idCustomer`, `shipperName`, `shipperPhoneNumber`, `shipCost`, `status`, `notice`, `startDate`, `endDate`) VALUES (?, ?, ?, ?, ?, ?, ?, current_timestamp(), ?)";
+        String query = "INSERT INTO `shipment` (`idOrder`, `idCustomer`, `shipperName`, `shipperPhoneNumber`, `shipCost`, `status`, `notice`, `startDate`) VALUES (?, ?, ?, ?, ?, ?, ?, current_timestamp())";
 
         PreparedStatement stmt = conn.prepareStatement(query);
         stmt.setInt(1, t.getIdOrder());
@@ -61,34 +61,51 @@ public class ShipmentDao extends Dao<Shipment> {
         stmt.setInt(5, t.getShipCost());
         stmt.setNString(6, t.getStatus().getId());
         stmt.setNString(7, t.getNotice());
-        stmt.setTimestamp(8, t.getStartDate());
-        stmt.setTimestamp(9, t.getEndDate());
         int row = stmt.executeUpdate();
     }
 
     @Override
     public void update(Shipment t) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (t == null) {
+            throw new SQLException("Shipment rỗng");
+        }
+        String query = "UPDATE `shipment` SET `idCustomer` = '2', `shipperName` = '?, `shipperPhoneNumber` = ?, `shipCost` = ?, `status` = ?, `notice` = ?, `startDate` = ?, `endDate` = ? WHERE `shipment`.`idOrder` = ?";
+
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setInt(1, t.getIdCustomer());
+        stmt.setNString(2, t.getShipperName());
+        stmt.setNString(3, t.getShipperPhoneNumber());
+        stmt.setInt(4, t.getShipCost());
+        stmt.setNString(5, t.getStatus().getId());
+        stmt.setNString(6, t.getNotice());
+        stmt.setTimestamp(7, t.getStartDate());
+        stmt.setTimestamp(8, t.getEndDate());
+        stmt.setInt(9, t.getIdOrder());
+        int row = stmt.executeUpdate();
     }
 
     @Override
     public void delete(Shipment t) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM `shipment` WHERE `shipment`.`idOrder` = ?");
+        stmt.setInt(1, t.getIdOrder());
+        stmt.executeUpdate();
     }
 
     @Override
     public void deleteById(int id) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM `shipment` WHERE `shipment`.`idOrder` = ?");
+        stmt.setInt(1, id);
+        stmt.executeUpdate();
     }
 
     public ArrayList<Shipment> searchByKey(String key, String word) throws SQLException {
         ArrayList<Shipment> shipments = new ArrayList<>();
         Statement statement = conn.createStatement();
-        String query = "SELECT * FROM Table WHERE " + key + " LIKE '%" + word + "%';";
+        String query = "SELECT * FROM `shipment` WHERE " + key + " LIKE '%" + word + "%';";
         ResultSet rs = statement.executeQuery(query);
         while (rs.next()) {
-            //  Shipment shipment= Shipment.getFromResultSet(rs);
-            // shipments.add(shipment);
+            Shipment shipment = Shipment.getFromResultSet(rs);
+            shipments.add(shipment);
         }
         return shipments;
     }
