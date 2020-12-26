e12abd75cf27be737ff013613b9f2d2f4c1aae7e
Trần Đức Cường <clonebmn2itt@gmail.com>
2020-12-26 11:11:54 +0700

    feat: update

--- src/main/java/dao/SessionDao.java
+++ src/main/java/dao/SessionDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -28,17 +29,40 @@ public class SessionDao extends Dao<Session> {
 
     @Override
     public Session get(int id) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Statement statement = conn.createStatement();
+        String query = "SELECT * FROM `session` WHERE `id` = " + id;
+        ResultSet rs = statement.executeQuery(query);
+        while (rs.next()) {
+            Session session = Session.getFromResultSet(rs);
+            return session;
+        }
+        return null;
     }
 
     @Override
     public void save(Session t) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (t == null) {
+            throw new SQLException("Shipment rỗng");
+        }
+        String query = "INSERT INTO `session` (`idEmployee`, `startTime`, `message`) VALUES (?, current_timestamp(), ?)";
+
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setInt(1, t.getIdEmployee());
+        stmt.setNString(2, t.getMessage());
+        int row = stmt.executeUpdate();
     }
 
     @Override
     public void update(Session t) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (t == null) {
+            throw new SQLException("Shipment rỗng");
+        }
+        String query = "UPDATE `session` SET `endTime` = current_timestamp(), `message` = ? WHERE `session`.`id` = ?";
+
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setNString(1, t.getMessage());
+        stmt.setInt(2, t.getIdEmployee());
+        int row = stmt.executeUpdate();
     }
 
     @Override
--- src/main/java/models/Session.java
+++ src/main/java/models/Session.java
@@ -10,66 +10,76 @@ import java.sql.Timestamp;
  * @author Đỗ Tuấn Anh <daclip26@gmail.com>
  */
 public class Session {
-
+    
     private int id, idEmployee;
     private Timestamp startTime, endTime;
     private Employee employee;
-
+    private String message;
+    
     public Session() {
     }
-
+    
     public int getId() {
         return id;
     }
-
+    
     public void setId(int id) {
         this.id = id;
     }
-
+    
     public Timestamp getStartTime() {
         return startTime;
     }
-
+    
     public Timestamp getEndTime() {
         return endTime;
     }
-
+    
     public Employee getEmployee() {
         return employee;
     }
-
+    
     public void setStartTime(Timestamp startTime) {
         this.startTime = startTime;
     }
-
+    
     public void setEndTime(Timestamp endTime) {
         this.endTime = endTime;
     }
-
+    
     public void setEmployee(Employee employee) {
         this.employee = employee;
     }
-
+    
     public int getIdEmployee() {
         return idEmployee;
     }
-
+    
     public void setIdEmployee(int idEmployee) {
         this.idEmployee = idEmployee;
     }
-
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
     public static Session getFromResultSet(ResultSet rs) throws SQLException {
         Session s = new Session();
         s.setId(rs.getInt("id"));
         s.setIdEmployee(rs.getInt("idEmployee"));
+        s.setMessage(rs.getNString("message"));
         s.setStartTime(rs.getTimestamp("startTime"));
         s.setEndTime(rs.getTimestamp("endTime"));
         return s;
     }
-
+    
     @Override
     public String toString() {
         return "Session{" + "id=" + id + ", idEmployee=" + idEmployee + ", startTime=" + startTime + ", endTime=" + endTime + ", employee=" + employee + '}';
     }
-
+    
 }
