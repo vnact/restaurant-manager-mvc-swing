9cec0c997a07edc004ad390d6c37171750b232bf
Trần Đức Cường <clonebmn2itt@gmail.com>
2020-12-30 18:37:41 +0700

    feat: update

--- src/main/java/controllers/admin/StatisticalController.java
+++ src/main/java/controllers/admin/StatisticalController.java
@@ -37,7 +37,7 @@ public class StatisticalController {
             view.getLbTotalOrder().setText(statisticalDao.getTotalOrder() + "");
             view.getLbTotalEmployee().setText(statisticalDao.getTotalEmployee() + "");
             view.getLbTotalCustomer().setText(statisticalDao.getTotalCustomer() + "");
-            IntervalIncrease.create(totalIncome, 3 * 1000, 25, (i) -> {
+            IntervalIncrease.create(totalIncome, 1500, 25, (i) -> {
                 view.getLbTotalIncome().setText(formatter.format(i));
             });
         } catch (Exception e) {
